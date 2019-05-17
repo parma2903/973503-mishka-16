ca196cb80bfb5621094401eeec5abb420887dc63
Yulia <parma2903@gmail.com>
2019-05-17 07:28:32 +0300

    Начало исправления формы

--- gulpfile.js
+++ gulpfile.js
@@ -98,6 +98,7 @@ gulp.task("copy", function () {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
     "source/img/**",
+    "source/css/**",
     "source/js/**",
     "source/*.ico"
   ], {
